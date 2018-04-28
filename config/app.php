b04e9998fe7041d520a8e81b9bb07c31001e1586
CROR3D <sider3d@gmail.com>
2018-04-28 12:06:57 +0200

    Sentinel

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Centaur\CentaurServiceProvider::class,
 
         /*
          * Application Service Providers...
